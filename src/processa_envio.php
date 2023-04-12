d7fa872078bf217ab26963f064787264a918db7a
Bruno Pierri <pierri95@gmail.com>
2023-04-11 22:53:15 -0300

    verficacao de post

--- src/processa_envio.php
+++ src/processa_envio.php
@@ -5,9 +5,12 @@ include 'Mensagem.php';
 use AppSendMail\Mensagem;
 
 $mensagem = new Mensagem();
+if(!empty($_POST))
+{
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
 
-$mensagem->__set('para', $_POST['para']);
-$mensagem->__set('assunto', $_POST['assunto']);
-$mensagem->__set('mensagem', $_POST['mensagem']);
+}
 
 $mensagem->enviarEmail();
